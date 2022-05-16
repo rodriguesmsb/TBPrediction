70c9273bea3821f9cfc768993ccd538b61655674
rodriguesmsb <rodriguesmsb@gmail.com>
2022-05-16 17:03:00 -0400

    test linear model

--- app/main.py
+++ app/main.py
@@ -1,24 +1,30 @@
 from flask import Flask
 from flask import request
 from flask import render_template
-
+import app.model_test as predictor
 
 
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
+@app.route("/", methods = ['GET','POST'])
+def make_pred():
+
+    if request.method == "POST":
+        age = request.form["age"]
+        prediction_result = predictor.point_prediction(int(age))
+        print(prediction_result)
+    
     return render_template("index.html")
     
 
-@app.route("/sub", methods = ['POST'])
-def submit():
-    #convert HTML info to python info
-    if request.method == "POST":
-        name = request.form["username"]
+# @app.route("/sub", methods = ['POST'])
+# def submit():
+#     #convert HTML info to python info
+#     if request.method == "POST":
+#         name = request.form["username"]
 
-    #python -> HTML
-    return render_template("sub.html", name = name)
+#     #python -> HTML
+#     return render_template("sub.html", name = name)
 
--- app/model.py
+++ app/model.py
@@ -1,2 +1,3 @@
 import pandas as pd
 from xgboost import XGBClassifier
+
