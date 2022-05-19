b6b712c5c2d74913c99c19d220040c07d2d7f674
rodriguesmsb <rodriguesmsb@gmail.com>
2022-05-19 16:15:23 -0400

    add form

--- app/model.py
+++ app/model.py
@@ -1,12 +1,6 @@
 import pandas as pd
 from xgboost import XGBClassifier
 
-
-
-
-
-
-
 def prediction_prob(idade, raca, sexo, prior_tb, hiv, diabetes, outra_disf, droga, alcool, fuma, bf, educacao = []):
 
     under_5, five_to_9, nine_to_12, more_than_12 = educacao
@@ -17,7 +11,7 @@ def prediction_prob(idade, raca, sexo, prior_tb, hiv, diabetes, outra_disf, drog
                                               "diabetes": [diabetes], "other_dishx": [outra_disf], "drug_yn": [droga], 
                                               "alcohol_yn": [alcool], "tobacco_yn": [fuma], "benef_gov": [bf], 
                                               "educ_cat_12+ years": [more_than_12], 
-                                            "educ_cat_5-9 years": [five_to_9], "educ_cat_9-12 years": [nine_to_12], "educ_cat_under_5": [under_5]})
+                                              "educ_cat_5-9 years": [five_to_9], "educ_cat_9-12 years": [nine_to_12], "educ_cat_under_5": [under_5]})
     return model.predict_proba(data_to_predict)[0][1]
 
 print(prediction_prob(18, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, [1, 0, 0, 0]))
