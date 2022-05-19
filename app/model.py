cb0c84f285ab79c0e5097604a303a8f677e35dd0
rodriguesmsb <rodriguesmsb@gmail.com>
2022-05-19 14:43:40 -0400

    add function to make prediction

--- app/model.py
+++ app/model.py
@@ -1,10 +1,23 @@
 import pandas as pd
 from xgboost import XGBClassifier
 
-model = XGBClassifier()
-model.load_model("model.json")
 
-data_to_predict = pd.DataFrame.from_dict({"idade": [69], "cs_raca": [1], "cs_sexo": [1], "prior_tb": [1], "hiv": [1], "diabetes": [1], "other_dishx": [1],
-                                          "drug_yn": [1], "alcohol_yn": [1], "tobacco_yn": [1], "benef_gov": [1], "educ_cat_12+ years": [0], 
-                                          "educ_cat_5-9 years": [0], "educ_cat_9-12 years": [0], "educ_cat_under_5": [0]})
-print(model.predict_proba(data_to_predict)[0][1])
+
+
+
+
+
+def prediction_prob(idade, raca, sexo, prior_tb, hiv, diabetes, outra_disf, droga, alcool, fuma, bf, educacao = []):
+
+    under_5, five_to_9, nine_to_12, more_than_12 = educacao
+    model = XGBClassifier()
+    model.load_model("model.json")
+
+    data_to_predict = pd.DataFrame.from_dict({"idade": [idade], "cs_raca": [raca], "cs_sexo": [sexo], "prior_tb": [prior_tb], "hiv": [hiv], 
+                                              "diabetes": [diabetes], "other_dishx": [outra_disf], "drug_yn": [droga], 
+                                              "alcohol_yn": [alcool], "tobacco_yn": [fuma], "benef_gov": [bf], 
+                                              "educ_cat_12+ years": [more_than_12], 
+                                            "educ_cat_5-9 years": [five_to_9], "educ_cat_9-12 years": [nine_to_12], "educ_cat_under_5": [under_5]})
+    return model.predict_proba(data_to_predict)[0][1]
+
+print(prediction_prob(18, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, [1, 0, 0, 0]))
