04396af030baa7490905cf1e3347dc07c1de27f3
rodriguesmsb <rodriguesmsb@gmail.com>
2023-04-03 15:12:57 -0400

    update

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,5 @@
 from app.main import app as application
-from app.main import app
 
-app = application
-# if __name__ == "__main__":
-#     main.app.run()
+# app = application
+if __name__ == "__main__":
+    application.run()
