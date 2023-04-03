8bb4d13a2437a0cfe25884e1af6849d380757e0b
rodriguesmsb <rodriguesmsb@gmail.com>
2023-04-03 15:13:51 -0400

    fix wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,15 @@
 from app.main import app as application
+#from app.main import app
 
-# app = application
-if __name__ == "__main__":
-    application.run()
+app = application
+# if __name__ == "__main__":
+#     main.app.run()
+
+
+
+
+# from app.main import app as application
+
+# # app = application
+# if __name__ == "__main__":
+#     application.run()
