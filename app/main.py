da0ce81b7f71f8f8b8f9d96a59a63b536bf254cd
rodriguesmsb <rodriguesmsb@gmail.com>
2024-02-16 14:46:56 -0400

    update graph template

--- app/main.py
+++ app/main.py
@@ -16,42 +16,42 @@ def make_pred():
     if request.method == "POST":
         #foo
 
-        result = request.form.to_dict(flat = False) #convert form to a dict
+        #result = request.form.to_dict(flat = False) #convert form to a dict
 
         #convert dict to a data frame
-        result = pd.DataFrame.from_dict(result)
-        for col in result.columns:
-            result[col] = result[col].astype(int)
+        #result = pd.DataFrame.from_dict(result)
+        #for col in result.columns:
+        #    result[col] = result[col].astype(int)
         
-        result.rename(columns = {"tb": "prior_tb", "drug":"ilicit_drug", "other":"other_dishx",
-                                 "alch":"alcohol", "tobaco":"tobacco",
-                                 "race": "race", "sex": "sex", "esc":"education",
-                                 "age": "age", "bf":"cash_transfer",
-                                 "jail": "liberty_dep", "hom": "exp_homelessness"}, inplace = True)
+        #result.rename(columns = {"tb": "prior_tb", "drug":"ilicit_drug", "other":"other_dishx",
+        #                         "alch":"alcohol", "tobaco":"tobacco",
+        #                         "race": "race", "sex": "sex", "esc":"education",
+        #                         "age": "age", "bf":"cash_transfer",
+        #                         "jail": "liberty_dep", "hom": "exp_homelessness"}, inplace = True)
 
 
         ##add new features
-        result = app_functions.create_feature(result)
+        #result = app_functions.create_feature(result)
     
         #remove other dishx
-        result.drop(["other_dishx"], axis = 1, inplace = True)
+        #result.drop(["other_dishx"], axis = 1, inplace = True)
 
         #reorder columns
-        result = result[['age', 'race', 'sex', 'education', 'prior_tb', 'hiv', 'diabetes',
-                         'ilicit_drug', 'alcohol', 'tobacco', 'cash_transfer', 'liberty_dep',
-                         'exp_homelessness', 'n_of_morb']]
+        #result = result[['age', 'race', 'sex', 'education', 'prior_tb', 'hiv', 'diabetes',
+        #                 'ilicit_drug', 'alcohol', 'tobacco', 'cash_transfer', 'liberty_dep',
+        #                 'exp_homelessness', 'n_of_morb']]
 
 
 
         
 
 
-        prob = app_functions.prediction_prob(result)[0]
+        #prob = app_functions.prediction_prob(result)[0]
 
         
         data = [
-            ("Desfavorável", prob),
-            ("Favorável", 1 - prob)
+            ("Desfavorável", 0.5),
+            ("Favorável", 1 - 0.5)
         ]
 
         #split data into two list
