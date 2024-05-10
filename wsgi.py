52488fb0670b0ff5b5029917b896740b8b4db5b8
rodriguesmsb <rodriguesmsb@gmail.com>
2024-05-10 15:24:05 -0400

    restore

--- wsgi.py
+++ wsgi.py
@@ -5,6 +5,6 @@ from app.main import app as application
 if __name__ == "__main__":
     application.run()
 
-""" # to deploy on render we should to use the follow structure in wsgi
-from app.main import app as application
+# to deploy on render we should to use the follow structure in wsgi
+""" from app.main import app as application
 app = application """
